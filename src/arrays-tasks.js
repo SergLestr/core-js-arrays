b7293eab49356f185f76108de69e869a9108effe
LestrSerg <lestrlearn@gmail.com>
2025-11-17 23:27:17 +0300

    4

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -26,7 +26,6 @@ function getIntervalArray(start, end) {
     res.push(start);
   } else {
     const step = start <= end ? 1 : -1;
-
     for (let i = start; step === 1 ? i <= end : i >= end; i += step) {
       res.push(i);
     }
@@ -47,8 +46,15 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  const result = [];
+  const maxLen = Math.max(arr1.length, arr2.length);
+
+  for (let i = 0; i < maxLen; i += 1) {
+    result[i] = (arr1[i] || 0) + (arr2[i] || 0);
+  }
+
+  return result;
 }
 
 /**
@@ -63,8 +69,8 @@ function sumArrays(/* arr1, arr2 */) {
  *    findElement(['Array', 'Number', 'string'], 'Date') => -1
  *    findElement([0, 1, 2, 3, 4, 5], 5) => 5
  */
-function findElement(/* arr, value */) {
-  throw new Error('Not implemented');
+function findElement(arr, value) {
+  return arr.indexOf(value);
 }
 
 /**
